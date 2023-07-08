31715ee2ffa8b081eaf92e5c2307f88aba07c84b
Akshat Jain <akshatjain0630@gmail.com>
2023-07-08 22:07:30 +0530

    Add files via upload

---
+++ install/install_modules.bat
@@ -0,0 +1,27 @@
+@echo off
+pip install async-generator==1.10
+pip install attrs==21.4.0
+pip install certifi==2021.10.8
+pip install cffi==1.15.0
+pip install charset-normalizer==2.0.11
+pip install colorama==0.4.4
+pip install configparser==5.2.0
+pip install crayons==0.4.0
+pip install cryptography==36.0.1
+pip install h11==0.13.0
+pip install idna==3.3
+pip install outcome==1.1.0
+pip install pycparser==2.21
+pip install pyOpenSSL==22.0.0
+pip install python-dotenv==0.21.0
+pip install requests==2.27.1
+pip install selenium==4.1.0
+pip install sniffio==1.2.0
+pip install sortedcontainers==2.4.0
+pip install trio==0.19.0
+pip install trio-websocket==0.9.2
+pip install urllib3==1.26.8
+pip install webdriver-manager==3.5.2
+pip install wsproto==1.0.0
+
+echo All required modules have been installed.
